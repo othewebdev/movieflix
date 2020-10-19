9d3203d6b70039844e419ea90df728faa76556b2
Odis Barnett <kingo.iv@gmail.com>
2020-10-19 00:36:29 -0500

    Updates

---
+++ precache-manifest.c39a7f4484c2ea2386546ecfd791365d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a07f023d701a846d80cd804b809d431",
+    "url": "/movieflix/index.html"
+  },
+  {
+    "revision": "9fba8c153c676c1c2972",
+    "url": "/movieflix/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "9fba8c153c676c1c2972",
+    "url": "/movieflix/static/js/2.99ade31c.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/movieflix/static/js/2.99ade31c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f4d809a1c2915a7b3c7",
+    "url": "/movieflix/static/js/main.dbefcb68.chunk.js"
+  },
+  {
+    "revision": "f3bf1dbe593867c02fdd",
+    "url": "/movieflix/static/js/runtime-main.1356559d.js"
+  },
+  {
+    "revision": "21111eec28084f80738920489a83dce5",
+    "url": "/movieflix/static/media/logo.21111eec.svg"
+  }
+]);
